f3280b8e53778d3e2f73fcb1de208958f0e52322
tpohling <tpohling@PC16143681>
2016-10-07 10:54:43 +0200

    Projektdateien hinzufügen.

---
+++ XML/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSpeichern = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseMove);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // buttonSpeichern
+            // 
+            this.buttonSpeichern.Location = new System.Drawing.Point(25, 183);
+            this.buttonSpeichern.Name = "buttonSpeichern";
+            this.buttonSpeichern.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpeichern.TabIndex = 2;
+            this.buttonSpeichern.Text = "Speichern";
+            this.buttonSpeichern.UseVisualStyleBackColor = true;
+            this.buttonSpeichern.Click += new System.EventHandler(this.buttonSpeichern_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Location = new System.Drawing.Point(202, 211);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(35, 13);
+            this.labelTimer.TabIndex = 3;
+            this.labelTimer.Text = "label1";
+            this.labelTimer.Click += new System.EventHandler(this.labelTimer_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(25, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.buttonSpeichern);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSpeichern;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ XML/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace XML
+{
+    public partial class Form1 : Form
+    {
+        TimeSpan Dauer = new TimeSpan(0, 0, 10);
+        DateTime Start;
+        public Form1()
+        {
+            InitializeComponent();
+            Start = DateTime.Now;
+            timer1.Start();
+        }
+
+
+        private void buttonSpeichern_Click(object sender, EventArgs e)
+        {
+            
+            StringWriter ausgabe = new StringWriter();
+
+            using (XmlWriter xmlAusgabe = XmlWriter.Create(Path.GetTempFileName(), new XmlWriterSettings() { Indent = true }))
+            {
+                try
+                {
+                    //xmlAusgabe.WriteStartDocument();
+                    //xmlAusgabe.WriteStartElement("TEST");
+                    //xmlAusgabe.WriteStartElement("TEST", textBox1.Text);
+                    //xmlAusgabe.WriteStartElement("TEST", richTextBox1.Text);
+                    //xmlAusgabe.WriteEndElement();
+                }
+                finally
+                {
+                    xmlAusgabe.Flush();
+                }
+            }
+            //richTextBoxAusgabe.Text = ausgabe.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan diff = DateTime.Now.Subtract(Start);
+            labelTimer.Text = diff.TotalSeconds.ToString();
+            if (Dauer < diff)
+            {
+                panel1.Enabled = false;
+
+                // 
+                // Thread Daten laden
+                // Benutzer deaktiviern
+                // Daten anzeigen
+                // Benutzer aktivieren
+            }
+
+
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            Start = DateTime.Now;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Start = DateTime.Now;
+
+        }
+
+        private void textBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Start = DateTime.Now;
+
+        }
+
+        private void labelTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
